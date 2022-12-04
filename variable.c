78458c22e25abe5e27260a8dbb14c95d115239b3
AyushAwasthi1999 <ayushawasthi504@gmail.com>
2022-12-04 19:14:48 +0530

    c notes

---
+++ variable.c
@@ -0,0 +1,11 @@
+// what IS A VARIABLE ?
+
+//ANSWER :-  A name given to memory location
+// Declared by writing type variable_name;
+// Initialized and declared by type variable_name = value;
+
+
+// RULES OF DEFINING VARIABLE IN C .
+
+// Can't start with a digit
+// No whitespace and reserved keyword is allowed
