fcce896e7e6165f9d3c681684bf2c5a1aa92afd5
AyushAwasthi1999 <ayushawasthi504@gmail.com>
2022-12-09 10:59:15 +0530

    c_notes

---
+++ switch_case.c
@@ -0,0 +1,12 @@
+// Switch Statement
+// Instead of writing many if..else statements, you can use the switch statement.
+
+/*
+    This is how it works:
+
+        The switch expression is evaluated once
+        The value of the expression is compared with the values of each case
+        If there is a match, the associated block of code is executed
+        The break statement breaks out of the switch block and stops the execution
+        The default statement is optional, and specifies some code to run if there is no case match
+*/
